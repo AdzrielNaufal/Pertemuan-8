e508010bb0826514dcb6d39c6492ab5abf6fcf23
AdzrielNaufal <adzrielnaufal81@gmail.com>
2024-05-30 14:56:29 +0700

    Pertemuan 8

---
+++ vue-project-praktikum-8/src/components/Artikel.vue
@@ -0,0 +1,7 @@
+<template>
+    <div>
+    <h1>Nasi bungkus 2</h1>
+    <p>saya orang 2 saya 1 sama k.... 1</p>
+    </div>
+    <img src="../assets/GHe4ETLakAAj9sY.jpg" alt="Panitia Bobrok" width="400px" height="auto">
+</template>
